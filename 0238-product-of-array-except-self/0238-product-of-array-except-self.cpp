076ed432c8e6600428e23c5de19d26d5269cacf6
kareemabdeen <118139061+kareemabdeen@users.noreply.github.com>
2023-07-23 17:06:17 +0300

    Time: 29 ms (59.21%), Space: 25 MB (36.47%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
+vector<int> productExceptSelf(vector<int>& nums) {
 
-        const int size=nums.size();
-        vector<int>prefixVector(size);
-        vector<int>suffixVector(size);
+    const int size = nums.size();
+    vector<int> prefixVector(size);
+    vector<int> suffixVector(size);
+    vector<int> answer;
 
-        prefixVector[0]=nums[0];
-        for(int i=1;i<=nums.size()-1;i++)
-            prefixVector[i]=prefixVector[i-1]*nums[i];
+    // reserve space for the answer vector
+    answer.reserve(size);
 
-
-
-        suffixVector[size-1]=nums[size-1];
-        for(int i=size-2;i>=0 ;--i)
-            suffixVector[i]=suffixVector[i+1]*nums[i];
-
-        vector<int>answer(size);
-        for(int i=0;i<=size-1;i++) {
-            if (i == 0)
-                answer[i]=suffixVector[i + 1];
-            else if (i == size - 1)
-                answer[i]=(prefixVector[i - 1]);
-            else
-                answer[i]=suffixVector[i + 1] * prefixVector[i - 1];
-        }
-        return answer;
+    prefixVector[0] = nums[0];
+    for (int i = 1; i < size; i++) {
+        prefixVector[i] = prefixVector[i-1] * nums[i];
     }
+
+    suffixVector[size-1] = nums[size-1];
+    for (int i = size-2; i >= 0; i--) {
+        suffixVector[i] = suffixVector[i+1] * nums[i];
+    }
+
+    for (int i = 0; i < size; i++) {
+        if (i == 0) {
+            answer.push_back(suffixVector[i+1]);
+        } else if (i == size-1) {
+            answer.push_back(prefixVector[i-1]);
+        } else {
+            answer.push_back(prefixVector[i-1] * suffixVector[i+1]);
+        }
+    }
+
+    return answer;
+}
 };
