b5958b01a6599032f67a9e61c35241c4386d8a03
kareemabdeen <118139061+kareemabdeen@users.noreply.github.com>
2023-08-02 19:57:50 +0300

    Time: 24 ms (64.26%), Space: 20.6 MB (17.58%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,31 +1,33 @@
+#include <vector>
+#include <set>
+#include <map>
+
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-
         vector<set<int>> row(9);
-        vector<set<int>> coulmn(9);
-        int subblock[3][3][10]={0}; // 3*3 sub block with 10 elements inside
-        int value=0;
-        for(int i=0;i<9 ;i++)
-        {
-            for(int j=0;j<9;j++)
-            {
-                if(board[i][j]=='.')
-                    continue;
+        vector<set<int>> column(9);
+        map<pair<int, int>, set<int>> subblock;
 
-                value=board[i][j]-'0';
-                if(subblock[i/3][j/3][value]==1 || row[i].count(value)==1 || coulmn[j].count(value)==1 ) // false
-                     return false ;
-                else {
-                    row[i].insert(value);
-                    coulmn[j].insert(value);
-                    subblock[i/3][j/3][value]=1 ; 
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    continue;
                 }
 
+                int value = board[i][j] - '0';
+
+                // Check if the value already exists in the same row, column or sub-block
+                if (row[i].count(value) == 1 || column[j].count(value) == 1 || subblock[{i/3, j/3}].count(value) == 1) {
+                    return false;
+                } else {
+                    row[i].insert(value);
+                    column[j].insert(value);
+                    subblock[{i/3, j/3}].insert(value);
+                }
             }
         }
+
         return true;
-
-
     }
 };
